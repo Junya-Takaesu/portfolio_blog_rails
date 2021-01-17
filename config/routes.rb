dffc1af110c79ac8f7579691c5555e3b3020e084
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-01-17 17:41:24 +0900

    Modify routes for artilces url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_articles/index'
-  get 'static_articles/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'articles', to: 'static_articles#index'
+  get 'articles/:id', to: 'static_articles#show'
 end
