c4450958f12371df1d7ca18c336bc40758973fa7
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-01-17 19:56:27 +0900

    Add statice pages

--- app/controllers/static_articles_controller.rb
+++ app/controllers/static_articles_controller.rb
@@ -1,7 +1,12 @@
 class StaticArticlesController < ApplicationController
   def index
+    @articles = [
+      {id: 1, title: "初めての記事"},
+      {id: 2, title: "2番目の記事"}
+    ]
   end
 
   def show
+    render "pages/article#{params[:id]}"
   end
 end
