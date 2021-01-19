b165f7cd8e5e6a10c5e4a0f2372dbb5267f47996
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-01-19 16:50:57 +0900

    Update the format of the age

--- app/helpers/static_articles_helper.rb
+++ app/helpers/static_articles_helper.rb
@@ -1,2 +1,8 @@
 module StaticArticlesHelper
+  require 'dotiw'
+  include ActionView::Helpers::DateHelper
+
+  def age_distance_of_time_in_words(date_of_birth: Time.gm(1993, 4, 7))
+    distance_of_time_in_words(Time.now, date_of_birth)
+  end
 end
