9d2b6d1e4ecb43984b8531bf7ce18eef21ae2736
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-01-25 16:11:10 +0900

    Display modified date of the current article dynamically

--- app/helpers/static_articles_helper.rb
+++ app/helpers/static_articles_helper.rb
@@ -5,4 +5,8 @@ module StaticArticlesHelper
   def age_distance_of_time_in_words(date_of_birth: Time.gm(1993, 4, 7))
     distance_of_time_in_words(Time.now, date_of_birth)
   end
+
+  def get_modified_date(file)
+    File.mtime("#{File.dirname(file)}/_article#{params[:id]}.html.erb")
+  end
 end
