59503b5af845497c1088f98eb1ccaef3cd35a8f4
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-01-25 15:38:38 +0900

    Change hero image of an article dynamically

--- app/controllers/static_articles_controller.rb
+++ app/controllers/static_articles_controller.rb
@@ -13,8 +13,8 @@ class StaticArticlesController < ApplicationController
   private
     def set_atricles
       @articles = [
-        {id: 1, title: "初めての記事"},
-        {id: 2, title: "Webpack と CDN の比較"}
+        {id: 1, title: "初めての記事", hero_image: "hero-article1.jpg"},
+        {id: 2, title: "Webpack と CDN の比較", hero_image: "hero-article2.png"}
       ]
     end
 end
