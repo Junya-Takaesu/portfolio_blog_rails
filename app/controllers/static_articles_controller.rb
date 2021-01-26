71169f5a3716b9bf6590936755b01c1f1346fa2c
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-01-26 09:02:59 +0900

    Write article3

--- app/controllers/static_articles_controller.rb
+++ app/controllers/static_articles_controller.rb
@@ -15,7 +15,8 @@ class StaticArticlesController < ApplicationController
     def set_atricles
       @articles = [
         {id: 1, title: "初めての記事", hero_image: "hero-article1.jpg"},
-        {id: 2, title: "Webpack と CDN の比較", hero_image: "hero-article2.png"}
+        {id: 2, title: "Webpack と CDN の比較", hero_image: "hero-article2.png"},
+        {id: 3, title: "Rails のレンダリングについて (View の表示)", hero_image: "hero-article3.jpg"},
       ]
     end
 end
