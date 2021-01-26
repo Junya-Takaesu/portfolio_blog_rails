dc979dfa5c57d4a471725789d98182c122f9e8a9
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-01-26 21:00:30 +0900

    Add article 5 and Update article 4

--- app/controllers/static_articles_controller.rb
+++ app/controllers/static_articles_controller.rb
@@ -18,6 +18,7 @@ class StaticArticlesController < ApplicationController
         {id: 2, title: "Webpack と CDN の比較", hero_image: "hero-article2.png"},
         {id: 3, title: "Rails のレンダリングについて (View の表示)", hero_image: "hero-article3.jpg"},
         {id: 4, title: "コードスニペットをブログに取り入れる", hero_image: "markus-spiske-cvBBO4PzWPg-unsplash.jpg"},
+        {id: 5, title: "画面の幅がブレイクポイントに達したら、レイアウトを変える", hero_image: "ezgif-3-3463b321f157.gif"},
       ]
     end
 end
