98bfe3782626634cb36a28d7c19a710682c5b12e
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-01 00:25:17 +0900

    Revert bug management

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   root 'static_articles#about'
   get 'articles', to: 'static_articles#index'
   get 'articles/:id', to: 'static_articles#show'
-  get 'bugs', to: 'static_articles#bugs' # just for me
 end
