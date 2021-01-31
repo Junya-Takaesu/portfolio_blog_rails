c41b8ced1972a484d674d283fa2aa336c05b868f
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-01-31 21:42:40 +0900

    Add article8

--- app/controllers/static_articles_controller.rb
+++ app/controllers/static_articles_controller.rb
@@ -21,6 +21,7 @@ class StaticArticlesController < ApplicationController
         {id: 5, title: "画面の幅がブレイクポイントに達したら、レイアウトを変える", hero_image: "5/ezgif-3-3463b321f157.gif"},
         {id: 6, title: "ruby で、postgresql のテーブルを検索するやり方", hero_image: "6/luke-chesser-JKUTrJ4vK00-unsplash.jpg"},
         {id: 7, title: "bootstrap 5 の justify-content utilities を使う", hero_image: "7/bootstrap.jpg"},
+        {id: 8, title: "CSS練習(flex-box, 背景画像の設定, 疑似要素による明るさ設定)", hero_image: "8/flex-box-practice.jpg"},
       ]
     end
 end
