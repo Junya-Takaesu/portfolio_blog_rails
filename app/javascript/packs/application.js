762c44c09b0f5cb0cdec8d92459787ac8f7ffd88
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-01 16:04:21 +0900

    Restructored directories

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require("./about/index")
+require("./static_articles/about/index")
 
 Rails.start()
 Turbolinks.start()
