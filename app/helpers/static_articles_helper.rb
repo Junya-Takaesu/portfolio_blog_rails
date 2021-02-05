63142586dcab279d9dde0a49d918c29258b11f2d
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-05 16:36:00 +0900

    Separete logic from footer navigation

--- app/helpers/static_articles_helper.rb
+++ app/helpers/static_articles_helper.rb
@@ -1,3 +1,22 @@
 module StaticArticlesHelper
 
+  def generate_navigation_anchor(articles, current_article_id, direction)
+    directions = {prev: -1, next: 1}
+
+    article = articles.select {|article| article[:id] == (current_article_id + directions[direction.to_sym])}
+
+    if article.empty?
+      link_label = "記事がありません"
+      href = "#"
+      link_to link_label, href, class: "btn btn-outline-secondary col-12 disabled"
+    else
+      if direction == "prev"
+        link_label = "<<< 前の記事: #{article[0][:title]}"
+      else
+        link_label = "#{article[0][:title]} :次の記事 >>>"
+      end
+      href = article[0][:id].to_s
+      link_to link_label, href, class: "btn btn-outline-warning col-12", role: "button"
+    end
+  end
 end
