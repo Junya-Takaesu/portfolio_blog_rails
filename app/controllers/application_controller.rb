19c8f9981be4b423cc78f7cfd9766678c7706871
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-05 17:28:12 +0900

    Add parameters to markdown parser

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,28 +6,36 @@ class ApplicationController < ActionController::Base
     escape_html: true,
     hard_wrap: true,
     with_toc_data: true,
-    link_attributes: {target: "_blank"}
+    link_attributes: {target: "_blank"},
+    autolink: true,
+    fenced_code_blocks: true,
+    tables: true,
+    underline: true,
+    strikethrough: true,
+    disable_indented_code_blocks: true,
+    highlight: true,
+    footnotes: true
   )
     return "" if file_path.empty?
     renderer = Redcarpet::Render::HTML.new(
       render_options = {
-        escape_html: :escape_html,
-        hard_wrap: :hard_wrap,
-        with_toc_data: :with_toc_data,
-        link_attributes: {target: "_blank"}
+        escape_html: escape_html,
+        hard_wrap: hard_wrap,
+        with_toc_data: with_toc_data,
+        link_attributes: link_attributes,
       },
     )
     markdown = Redcarpet::Markdown.new(
       renderer,
       extensions = {
-        autolink: true,
-        fenced_code_blocks: true,
-        tables: true,
-        underline: true,
-        strikethrough: true,
-        disable_indented_code_blocks: true,
-        highlight: true,
-        footnotes: true
+        autolink: autolink,
+        fenced_code_blocks: fenced_code_blocks,
+        tables: tables,
+        underline: underline,
+        strikethrough: strikethrough,
+        disable_indented_code_blocks: disable_indented_code_blocks,
+        highlight: highlight,
+        footnotes: footnotes,
       },
     )
     markdown.render(File.read(file_path))
