3e1e6ee49cfb4634df8f184f99eda2b4122a0b7b
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-05 13:45:07 +0900

    Remove unused method

--- app/helpers/static_articles_helper.rb
+++ app/helpers/static_articles_helper.rb
@@ -1,9 +1,3 @@
 module StaticArticlesHelper
-  require 'dotiw'
-  include ActionView::Helpers::DateHelper
-
-  def age_distance_of_time_in_words(date_of_birth: Time.gm(1993, 4, 7))
-    distance_of_time_in_words(Time.now, date_of_birth)
-  end
 
 end
