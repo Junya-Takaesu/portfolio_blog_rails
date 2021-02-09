b79cd98300d31c4ceefa11766bd4f41edc0ffb21
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-10 00:56:55 +0900

    Fix everything related to articles

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,24 +1,21 @@
 module ArticlesHelper
-include Markdown::MarkdownParser
+  include Markdown::MarkdownParser
 
-  def generate_navigation_anchor(articles, current_article_id, direction)
-    directions = {prev: -1, next: 1}
+  def generate_navigation_anchor(article: , direction:)
+    class_attributes = "btn btn-outline-warning col-12"
 
-    # articles を総当りで見ているので、計算量が多い
-    article = articles.select {|article| article[:id] == (current_article_id + directions[direction.to_sym])}
-
-    if article.empty?
+    if direction == "prev" && article.prev_id
+      link_label = "<<< 前の記事: #{article.title}"
+      href = article.prev_id.to_s
+    elsif direction == "next" && article.next_id
+      link_label = "#{article.title} :次の記事 >>>"
+      href = article.next_id.to_s
+    else
       link_label = "記事がありません"
       href = "#"
-      link_to link_label, href, class: "btn btn-outline-secondary col-12 disabled"
-    else
-      if direction == "prev"
-        link_label = "<<< 前の記事: #{article[0][:title]}"
-      else
-        link_label = "#{article[0][:title]} :次の記事 >>>"
-      end
-      href = article[0][:id].to_s
-      link_to link_label, href, class: "btn btn-outline-warning col-12", role: "button"
+      class_attributes = "btn btn-outline-secondary col-12 disabled"
     end
+
+    link_to link_label, href, class: class_attributes, role: "button"
   end
 end
