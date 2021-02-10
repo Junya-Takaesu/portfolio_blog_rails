d6babdfbf9fb1f45852324a6a8edc79c1cd84c72
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-02-10 13:06:17 +0900

    Install prism code highlight

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 require("./articles/about/")
+require("./prism/prism")
 
 Rails.start()
 Turbolinks.start()
