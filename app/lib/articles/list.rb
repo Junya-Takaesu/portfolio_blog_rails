ba8cd0f96f63d9f08cb015ef713c8ce7a67f7380
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-03-07 23:23:11 +0900

    Simplify articles by removing "article" from the file names

--- app/lib/articles/list.rb
+++ app/lib/articles/list.rb
@@ -3,7 +3,7 @@ class Articles::List
 
   articles = JSON.parse(File.read("#{Rails.root}/app/views/articles/articles.json"))
   articles_array = articles.map do |key, value|
-    [key.to_i, Articles::Article.new(id: value["id"], title: value["title"], article_path: value["article_path"])]
+    [key.to_i, Articles::Article.new(id: value["id"], title: value["title"])]
   end
 
   ARTICLES_BY_ID = articles_array.to_h
