a727c5c3f7ec93f3db64a283b77f3636e9cce978
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-04-17 23:23:54 +0900

    Move codes from class scope to its initialize method

--- app/lib/articles/list.rb
+++ app/lib/articles/list.rb
@@ -3,14 +3,15 @@ require "json"
 class Articles::List
 
 
-  articles = JSON.parse(File.read("#{Rails.root}/app/views/articles/articles.json"))
-  articles_array = articles.map do |key, value|
-    [key.to_i, Articles::Article.new(id: value["id"], title: value["title"], created_at: value["created_at"])]
-  end
-
-  ARTICLES_BY_ID = articles_array.to_h
-
   def initialize
+    articles_json_parsed = JSON.parse(File.read("#{Rails.root}/app/views/articles/articles.json"))
+    articles_array = articles_json_parsed.map do |key, value|
+      [key.to_i, Articles::Article.new(id: value["id"], title: value["title"], created_at: value["created_at"])]
+    end
+
+    @articles_hash = articles_array.to_h
+    self
   end
+
 
 end
