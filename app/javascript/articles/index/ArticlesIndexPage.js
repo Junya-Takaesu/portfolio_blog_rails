c49dd136cea573a2cfb1f0bf87317b0fc6a1e76e
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-04-20 23:10:24 +0900

    Add animated hamburger slide menu

--- app/javascript/articles/index/ArticlesIndexPage.js
+++ app/javascript/articles/index/ArticlesIndexPage.js
@@ -1,45 +1,107 @@
 export default class ArticlesIndexPage {
+  constructor() {
+    this.filterMenuId = "filter";
+    this.filterHeaders = document.querySelectorAll(".filter-header");
+    this.filterMenu = document.querySelector(`#${this.filterMenuId}`);
+    this.filterItems = document.querySelectorAll(".filter-item");
+    this.burgerIconId = "burger";
+    this.burgerIcon = document.querySelector(`#${this.burgerIconId}`);
+    this.eventTarget = null;
+  }
+
   initialize() {
     this.burger();
   }
 
   burger() {
-    const burgerIconId = "burger";
-    const filterMenuId = "filter";
-    const burgerIcon = document.querySelector(`#${burgerIconId}`);
-    const filterMenu = document.querySelector(`#${filterMenuId}`);
-    const filterItems = document.querySelectorAll(".filter-item");
-    const filterHeaders = document.querySelectorAll(".filter-header");
+    const body = document.querySelector("body");
 
-    document.addEventListener("click", event => {
-      const eventTargetId = event.target.id;
-      const eventTargetParentId = event.target.parentElement.id;
+    body.addEventListener("click", event => {
+      this.eventTarget = event.target;
 
-      if (eventTargetId == burgerIconId || eventTargetParentId == burgerIconId) {
-        filterMenu.classList.toggle("active");
-        this.toggleCascadingAnimation(filterItems, 8)
-        this.toggleCascadingAnimation(filterHeaders, 2);
-      } else if(eventTargetId != filterMenuId) {
-        filterMenu.classList.remove("active");
-        filterItems.forEach(filterItem => this.resetAnimation(filterItem));
-        filterHeaders.forEach(filterHeader => this.resetAnimation(filterHeader));
+      if (this.isBurgerClicked()) {
+        this.burgerIcon.classList.toggle("active");
+        this.toggleFilterMenu();
+      } else if (this.isOutsideOfMenuClicked()) {
+        this.burgerIcon.classList.remove("active");
+        this.hideFilterMenu();
       }
     });
   }
 
-  toggleCascadingAnimation(targets, delayDivider) {
+  isBurgerClicked() {
+    return this.eventTarget.id == this.burgerIconId || this.eventTarget.parentElement.id == this.burgerIconId
+  }
+
+  isOutsideOfMenuClicked() {
+    return this.eventTarget.id != this.filterMenuId && this.eventTarget.parentElement.id != this.filterMenuId && this.eventTarget.tagName.toLowerCase() != "i"
+  }
+
+  toggleFilterMenu() {
+    this.filterHeaders.forEach(subHeader => {
+      this.filterMenu.append(this.createTopLevelMenuHeader(subHeader));
+    });
+
+    this.filterMenu.classList.toggle("active");
+    if (!this.filterMenu.classList.contains("active")) {
+      this.resetFilterMenu();
+    }
+  }
+
+  hideFilterMenu() {
+    this.filterMenu.classList.remove("active");
+    this.filterItems.forEach(filterItem => filterItem.style.animation = "");
+    this.filterHeaders.forEach(subHeader => {
+      subHeader.style.animation = "";
+      subHeader.parentElement.classList.remove("active");
+    });
+
+    this.resetFilterMenu();
+  }
+
+  resetFilterMenu() {
+    this.removeTopLevelMenuHeaders();
+    this.filterHeaders.forEach(subHeader => {
+      subHeader.parentElement.classList.remove("active");
+    });
+
+    this.filterHeaders.forEach(filterHeader => filterHeader.style.animation = "");
+    this.filterItems.forEach(filterItem => filterItem.style.animation = "");
+  }
+
+  removeTopLevelMenuHeaders() {
+    document.querySelectorAll(".top-level-menu-item").forEach(dom => {
+      dom.remove();
+    });
+  }
+
+  createTopLevelMenuHeader(subHeader) {
+    const topLevelMenuParagraph = document.createElement("p");
+    topLevelMenuParagraph.innerHTML = `${subHeader.innerText}<br>で記事を絞る <i class="bi bi-chevron-compact-right"></i>`;
+    topLevelMenuParagraph.classList.add("top-level-menu-item");
+
+    topLevelMenuParagraph.addEventListener("click", () => {
+      const filterHeader = subHeader.parentElement.querySelector(".filter-header") ;
+      const filterItems = subHeader.parentElement.querySelectorAll(".filter-item");
+      let delayGranularityForHeader = 1;
+      let delayGranularityForItems = 40;
+
+      subHeader.parentElement.classList.toggle("active");
+
+      this.toggleCascadingAnimation([filterHeader], delayGranularityForHeader);
+      this.toggleCascadingAnimation(filterItems, delayGranularityForItems);
+    });
+
+    return topLevelMenuParagraph;
+  }
+
+  toggleCascadingAnimation(targets, delayGranularity) {
     targets.forEach((target, index) => {
       if (target.style.animation) {
         target.style.animation = "";
       } else {
-        target.style.animation = `navLinkFade 0.5s ease forwards ${index / delayDivider}s`;
+        target.style.animation = `navLinkFade 0.5s ease forwards ${index / delayGranularity}s`;
       }
     });
-  }
-
-  resetAnimation(target) {
-    if (target.style.animation) {
-      target.style.animation = "";
-    }
   }
 }
