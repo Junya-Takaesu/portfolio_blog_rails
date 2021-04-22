b9233346309e2b8d5a1602a3e8694a9643b7a2f9
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-04-23 00:12:26 +0900

    Add filter method to Articles::List class

--- app/lib/articles/list.rb
+++ app/lib/articles/list.rb
@@ -2,7 +2,7 @@ require "json"
 
 class Articles::List
 
-  attr_reader :all, :tags
+  attr_reader :all, :filtered, :tags
 
   Order = ["asc", "desc"]
   Article = Articles::Article
@@ -16,11 +16,68 @@ class Articles::List
     end
 
     @all = articles_array.to_h
+    @filtered = {}
     @tags = get_tags
     @last_called_method = __method__
     self
   end
 
+  def filter(condition = {})
+    filtered_by = {
+      CreatedAt => {},
+      Tags => {}
+    }
+
+    condition.each do |key, value|
+      case key
+      when CreatedAt
+        param_created_at = value
+        param_created_at.each do |_param_created_at|
+          _filtered_by_created_at = @all.select do |_key, _article|
+            _article.to_h[CreatedAt].month == Date.parse(_param_created_at.strip).month
+          end
+          filtered_by[CreatedAt].merge!(_filtered_by_created_at)
+        end
+      when Tags
+        article_keys_filtered_by_tags = []
+        common_articles_indexes = []
+        param_tags = value
+
+        param_tags.each_with_index do |_param_tag, _index|
+          _filtered_by_tag = @all.select do |_key, _article|
+            _article.to_h[Tags].include?(_param_tag.strip)
+          end
+          article_keys_filtered_by_tags.append(_filtered_by_tag.keys)
+        end
+
+        if 0 < param_tags.size
+          common_articles_indexes = article_keys_filtered_by_tags.first
+          article_keys_filtered_by_tags.each_with_index do |_indexes, _index|
+            common_articles_indexes = common_articles_indexes & _indexes
+          end
+        end
+
+        filtered_by[Tags] = @all.select {|_index, _article| common_articles_indexes.include?(_index)}
+      else
+        return self
+      end
+    end
+
+    is_created_at_given = !condition[CreatedAt].empty?
+    is_tags_given = !condition[Tags].empty?
+
+    if !is_tags_given && is_created_at_given
+      @filtered = filtered_by[CreatedAt]
+    elsif is_tags_given && !is_created_at_given
+      @filtered = filtered_by[Tags]
+    elsif is_tags_given && is_created_at_given
+      result_keys = filtered_by[CreatedAt].keys & filtered_by[Tags].keys
+      @filtered = @all.select {|_index, _article| result_keys.include?(_index)}
+    end
+
+    @last_called_method = __method__
+    self
+  end
 
   def sort(sort_key: "created_at", order: "desc")
     if !Article::Properties.include?(sort_key) || !Order.include?(order)
