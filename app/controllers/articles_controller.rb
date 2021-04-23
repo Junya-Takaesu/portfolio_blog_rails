9fe3a68e4f73825df54fef08b8ffa9701abc9ca9
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-04-23 12:00:51 +0900

    Connect filter UI to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,30 @@
 class ArticlesController < ApplicationController
 
   ArticlesList = Articles::List
+  Article = Articles::Article
 
   def index
     sort_key = params[:sort_key].nil? ? "created_at" : params[:sort_key]
     order = params[:order].nil? ? "desc" : params[:order]
+    @param_tags       = params[:tags].nil?       ? ""           : params[:tags]
+    @param_created_at = params[:created_at].nil? ? ""           : params[:created_at]
+
+    @param_tags = @param_tags.split(",")
+    @param_created_at = @param_created_at.split(",").map {|year_month| "#{year_month}-01"}
 
     articles_list = ArticlesList.new
 
-    @articles = articles_list.sort(sort_key: sort_key, order: order).all
     @tags = articles_list.tags
+    @dates = articles_list.dates.map {|date| "#{date.year}-#{date.month.to_s.rjust(2, "0")}"}.uniq
+
+    if @param_tags.empty? && @param_created_at.empty?
+      @articles = articles_list.sort(sort_key: sort_key, order: order).all
+    else
+      condition = {Article::CreatedAt => @param_created_at, Article::Tags => @param_tags}
+      @articles = articles_list.filter(condition).sort(sort_key: sort_key, order: order).filtered
+    end
+
+    @param_created_at.map.with_index {|_date, index| @param_created_at[index] = _date[0..-4]}
   end
 
   def show
